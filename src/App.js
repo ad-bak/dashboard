59751face95123b0ae7768175d6b2012a4931034
Adnan <adbak.dev@gmail.com>
2023-09-24 22:26:27 +0200

    amazing grid

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./scenes/dashboard";
 import Sidebar from "./scenes/global/Sidebar";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
 
 // import Team from "./components/scenes/team";
 // import Invoices from "./components/scenes/invoices";
@@ -34,8 +35,8 @@ function App() {
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/team" element={<Team />} />
                 <Route path="/contacts" element={<Contacts />} />
-                {/* 
                 <Route path="/invoices" element={<Invoices />} />
+                {/* 
                 <Route path="/form" element={<Form />} />
                 <Route path="/bar" element={<Bar />} />
                 <Route path="/pie" element={<Pie />} />
---
+++ src/scenes/invoices/index.jsx
@@ -0,0 +1,88 @@
+import { Box, Typography, useTheme } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataInvoices } from "../../data/mockData";
+
+import Header from "../../components/Header";
+
+const Invoices = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const handleSelection = (newSelection) => {
+    console.log("Selected rows:", newSelection);
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column-cell",
+    },
+
+    { field: "phone", headerName: "Phone Number", flex: 1 },
+    { field: "email", headerName: "Email", flex: 1 },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => (
+        <Typography color={colors.greenAccent[500]} sx={{ ml: "5px" }}>
+          {params.value}
+        </Typography>
+      ),
+    },
+    { field: "date", headerName: "Date", flex: 1 },
+  ];
+
+  return (
+    <Box m="20px">
+      <Header title="Invoices" subtitle="List of Invoices" />
+      <Box
+        m="40px 0 0 0"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "1px solid #2a2a2a",
+            borderRight: `1px solid ${colors.grey[700]}`,
+          },
+          "& .name-column--cell": {
+            color: colors.greenAccent[300],
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: colors.blueAccent[700],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: colors.primary[400],
+          },
+          "& .MuiDataGrid-footerContainer": {
+            borderTop: "none",
+            backgroundColor: colors.blueAccent[700],
+          },
+          "& .MuiCheckbox-root": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+          "& .MuiCheckbox-root.Mui-checked": {
+            color: `${colors.greenAccent[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          onRowSelectionModelChange={({ ...data }) => handleSelection(data)}
+          checkboxSelection
+          rows={mockDataInvoices}
+          columns={columns}
+          width="100%"
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Invoices;
